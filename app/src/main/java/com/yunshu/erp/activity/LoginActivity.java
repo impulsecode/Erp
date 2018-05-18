2a09f99c44034cbdceed58d5e0fdd40eaab2d7d2
huangsecan <realbird@qq.com>
2018-05-18 17:29:21 +0800

    develop

--- app/src/main/java/com/yunshu/erp/activity/LoginActivity.java
+++ app/src/main/java/com/yunshu/erp/activity/LoginActivity.java
@@ -1,12 +1,15 @@
 package com.yunshu.erp.activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.yunshu.base.BaseActivity;
 import com.yunshu.erp.R;
+import com.yunshu.utils.ToastUtils;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
@@ -28,5 +31,15 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         x.view().inject(this);
+    }
+
+    @Event(type = View.OnClickListener.class, value = R.id.button_login)
+    private void buttonLoginClick(View view) {
+        ToastUtils.show(this, "12345", 500);
+    }
+
+    @Event(value = R.id.button_exit)
+    private void buttonExitClick(View view) {
+        ToastUtils.show(this, "54321", 500);
     }
 }
